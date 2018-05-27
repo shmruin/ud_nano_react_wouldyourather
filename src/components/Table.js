e989c7a3897f33713f7adf0d51cf910e03c2f125
ruin09 <meme_hm@naver.com>
2018-05-27 20:59:18 +0900

    Complete Table & Pagination of leaderboard
    #TODO : Login process

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import BootstrapTable from 'react-bootstrap-table-next'
+import paginationFactory from 'react-bootstrap-table2-paginator'
 
 
 class Table extends Component {
@@ -9,52 +12,75 @@ class Table extends Component {
         authedUser: PropTypes.string.isRequired,
     }
 
+    state = {
+        products: [],
+        columns: [{
+            dataField: 'rank',
+            text: '#'
+        },
+        {
+            dataField: 'userid',
+            text: 'User Id',
+        }, {
+            dataField: 'askings',
+            text: '# of Askings',
+            sort: true,
+        }, {
+            dataField: 'answers',
+            text: '# of Answers',
+            sort: true,
+        }, {
+            dataField: 'total',
+            text: 'Total Score',
+            sort: true,
+        }]
+    }
+
+    componentDidMount() {
+
+        //fill the table with users data
+        this.makeTableData()
+    }
+
+    makeTableData = () => {
+        const { users, authedUser } = this.props
+
+        let result = Object.keys(users).sort((a, b) => {
+            return (users[a].questions.length + Object.keys(users[a].answers).length) <
+            (users[b].questions.length + Object.keys(users[b].answers).length)
+            ? true : false
+        }).map((id, index) => {
+            let numAskings = users[id].questions.length
+            let numAnswers = Object.keys(users[id].answers).length
+
+            return {
+                'rank': index+1,
+                'userid': id,
+                'askings': numAskings,
+                'answers': numAnswers,
+                'total': numAskings + numAnswers,
+            }
+        })
+
+        this.setState({
+            products: result,
+        })
+    }
+
     render() {
 
         const { users, authedUser } = this.props
 
         return (
-            <div className='container'>
-                <table className="table table-hover table-striped text-center">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>User Id</th>
-                            <th># of Askings</th>
-                            <th># of Answers</th>
-                            <th>Total Score</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {Object.keys(users).sort((a, b) => {
-                            return (users[a].questions.length + Object.keys(users[a].answers).length) <
-                            (users[b].questions.length + Object.keys(users[b].answers).length)
-                            ? true : false
-                        }).map((id, index) => {
-
-                            let numAskings = users[id].questions.length
-                            let numAnswers = Object.keys(users[id].answers).length
-
-                            return (<tr key={index}>
-                                <th scope="row">{index + 1}</th>
-                                <td>{id}</td>
-                                <td>{numAskings}</td>
-                                <td>{numAnswers}</td>
-                                <td>{numAskings + numAnswers}</td>
-                            </tr>)
-                        })}
-                    </tbody>
-                </table>
-                {/* TODO: Paginations */}
-                <nav className='mt-4'>
-                    <ul className="pagination justify-content-center">
-                        <li className="page-item disabled"><a className="page-link" href="#">Previous</a></li>
-                        <li className="page-item active"><a className="page-link" href="#">1</a></li>
-                        <li className="page-item"><a className="page-link" href="#">2</a></li>
-                        <li className="page-item"><a className="page-link" href="#">3</a></li>
-                        <li className="page-item"><a className="page-link" href="#">Next</a></li>
-                    </ul>
-                </nav>
+            <div className="container" style={{ marginTop: 50 }}>
+                <BootstrapTable
+                    striped
+                    hover
+                    keyField='id'
+                    data={this.state.products}
+                    columns={this.state.columns}
+                    pagination={paginationFactory()}
+                />
             </div>
         )
     }
